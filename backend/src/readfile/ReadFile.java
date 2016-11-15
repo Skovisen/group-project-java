26b47623eedce5469a2aa2187cf973dc2150d300
Skovisen <christianskovborg@outlook.com>
2016-11-15 20:13:15 +0100

    ReadFile was the wrong source file to begin with, updated to the correct source file. The code is now as it should be.

--- backend/src/readfile/ReadFile.java
+++ backend/src/readfile/ReadFile.java
@@ -1,25 +1,46 @@
-import java.io.*;
-import java.util.Scanner;
+package readfile;
 
-public class ReadFile
-{
+import javax.swing.*;
+import java.io.*;
+import java.util.*;
+
+public class ReadFile {
+    static JFrame parentFrame = new JFrame();
     private String filePath;
-    public static void main(String[] args)
-    {
+    static JFileChooser dialog = new JFileChooser();
+    public static void main(String[] args) {
+        dialog.showOpenDialog(parentFrame);
         try {
-            readFile("C:\\\\chris\\Desktop\\new.txt");
+            readFile(dialog.getSelectedFile().getAbsolutePath());
         } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-    public static void readFile(String path) throws FileNotFoundException
-    {
+    public static void readFile(String path) throws FileNotFoundException {
         Scanner fileIn = new Scanner(new File(path));
-        while (fileIn.hasNextLine())
-        {
-            System.out.println(fileIn.nextLine());
+        //læser filen
+        ArrayList<String> array_list = new ArrayList<>();
+        //laver en arraylist
+        while (fileIn.hasNextLine()) {
+            array_list.add(fileIn.nextLine());
+            //tilføjer hver linje i filen til arraylisten
         }
+        Collections.sort(array_list);
+        //sorterer arraylisten i alfabetisk orden
+        dialog.showSaveDialog(parentFrame);
+        try {
+            PrintWriter test = new PrintWriter(dialog.getSelectedFile().getAbsolutePath());
+            //laver ny fil
+            for (String item : array_list) {
+                test.println(item);
+                //tilføjer hver element i arraylisten til filen
+            }
+            test.close();
+
+        }   catch(FileNotFoundException e){
+            e.printStackTrace();
+        }
+
     }
 
 }
